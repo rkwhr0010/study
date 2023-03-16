f79bae2a65ab69b21a3da94b9f67cdf3d442095b
kks <rkwhr0010@naver.com>
2023-03-16 23:39:18 +0900

    이분검색 및 결정알고리즘

---
+++ src/sortsearching/뮤직비디오.java
@@ -0,0 +1,48 @@
+package sortsearching;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class 뮤직비디오 {
+	public static void main(String[] args) {
+		int[] arr = new Random().ints(20, 1, 6).toArray();
+		int musicLen = 20;
+		
+		//최소는 가장큰 한곡
+		int lt = Arrays.stream(arr).max().getAsInt();
+		//최대는 모든 곡길이 합
+		int rt = Arrays.stream(arr).sum();
+		
+		int answer = 0;
+		while(lt<=rt) {
+			//중각 곡 길이
+			int mid = (lt+rt)/2;
+			if(count(arr,mid)<=musicLen) {
+				// <= 부등호, 현재로선 최선의 답
+				answer = mid;
+				rt = mid-1;
+			}else {
+				lt = mid+1;
+			}
+			
+		}
+		System.out.println(answer);
+		
+		
+	}
+
+	private static int count(int[] arr, int capacity) {
+		int cnt = 1; 
+		int sum = 0;
+		for(int x : arr) {
+			if(sum+x>capacity) {
+				cnt++;
+				sum=x;
+			}else {
+				sum+=x;
+			}
+		}
+		
+		return cnt;
+	}
+}
---
+++ src/sortsearching/이분검색.java
@@ -0,0 +1,33 @@
+package sortsearching;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class 이분검색 {
+	
+	public static void main(String[] args) {
+		int[] arr = new Random().ints(20, 1, 100).distinct().toArray();
+		
+		int value = arr[10];
+		System.out.println(value);
+		//이분검색은 정렬이 필수
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		int lt = 0;
+		int rt = arr.length-1;
+		
+		while(lt<=rt) {
+			//중간 절삭
+			int mid = (lt+rt)/2;
+			if(arr[mid]==value) {
+				System.out.println(mid);
+				return;
+			}
+			if(arr[mid]<value) lt = mid+1;
+			else rt = mid-1;
+		}
+		
+	}
+	
+}
