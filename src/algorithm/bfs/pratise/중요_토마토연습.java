5b3c86dd1cb694ca6bef5a0034a10f76a7be56e2
kks <rkwhr0010@gamil.com>
2023-05-08 12:26:33 +0900

    BFS복습

--- src/algorithm/bfs/pratise/중요_토마토연습.java
+++ src/algorithm/bfs/pratise/중요_토마토연습.java
@@ -3,8 +3,9 @@ package algorithm.bfs.pratise;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.function.BiConsumer;
 
-public class 토마토연습 {
+public class 중요_토마토연습 {
 	static class Point{
 		public int x, y;
 		Point(int x, int y){
@@ -16,6 +17,7 @@ public class 토마토연습 {
 	static int[] dy={0, 1, 0, -1};
 	static int[][] board, dis;
 	static int n, m;
+	static boolean flag=true;
 	
 	static void B(Queue<Point> Q) {
 		while(!Q.isEmpty()) {
@@ -33,6 +35,26 @@ public class 토마토연습 {
 					//값 계산 핵심, 현위치 값에 +1
 					dis[nx][ny] = dis[now.x][now.y] + 1;
 					Q.offer(new Point(nx, ny));
+				}
+			}
+		}
+	}
+	
+	static void B2(Queue<Point> Q) {
+		while(!Q.isEmpty()) {
+			//현재 위치
+			Point cur = Q.poll();
+			
+			for (int i = 0; i < dx.length; i++) {
+				//다음 경로
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				//첫 방문 토마토
+				if(valid(nx, ny) && board[nx][ny] == 0) {
+					//방명록 작성
+					board[nx][ny] = 1;
+					dis[nx][ny] = dis[cur.x][cur.y]+1; // 현위치 +1
+					Q.offer(new Point(nx, ny));//현 상태값 저장
 				}
 			}
 		}
@@ -57,37 +79,51 @@ public class 토마토연습 {
 		
 		Queue<Point> queue = init(new LinkedList<>());
 		
-		B(queue);
-		for(int[] arr : dis) {
-			System.out.println(Arrays.toString(arr));
-		}
-		boolean flag=true;
-		int answer=Integer.MIN_VALUE;
+		B2(queue);
+		print(false);
+		
+		
+		int[] answer= {Integer.MIN_VALUE};
+		
 		for(int i=0; i<n; i++){
 			for(int j=0; j<m; j++){
 				if(board[i][j]==0) flag=false;
 			}
 		}
+		doubleEach((i,j)-> {if(board[i][j]==0) flag=false;} );
+		
 		if(flag){
-			for(int i=0; i<n; i++){
-				for(int j=0; j<m; j++){
-					answer=Math.max(answer, dis[i][j]);
-				}
-			}
-			System.out.println(answer);
+			doubleEach((i,j)-> {answer[0]=Math.max(answer[0], dis[i][j]);});
+			System.out.println(answer[0]);
 		}
 		else System.out.println(-1);
 	}
-
-	private static Queue<Point> init(Queue<Point> Q) {
+	
+	private static void doubleEach(BiConsumer<Integer, Integer> con) {
 		for(int i=0; i<n; i++){
 			for(int j=0; j<m; j++){
-				//시작점이 여러 개라 여기서 처리한다.
-				if(board[i][j]==1) Q.offer(new Point(i, j));
+				con.accept(i, j);
 			}
 		}
+	}
+
+	private static void print() {
+		print(true);
+	}
+	private static void print(boolean yn) {
+		if(yn) {
+			for(int[] arr : dis) {
+				System.out.println(Arrays.toString(arr));
+			}
+		}
+	}
+
+	private static Queue<Point> init(Queue<Point> Q) {
+		doubleEach((i,j)->{if(board[i][j]==1) Q.offer(new Point(i, j));});
 		return Q;
 	}
+	
+	
 }
 /*
 6 4
