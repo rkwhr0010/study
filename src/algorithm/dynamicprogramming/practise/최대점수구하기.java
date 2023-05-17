703a3adb235b24849d075cf036572fe7b9f1a2b9
kks <rkwhr0010@gamil.com>
2023-05-17 17:31:28 +0900

    최대점수구하기

--- src/algorithm/dynamicprogramming/practise/최대점수구하기.java
+++ src/algorithm/dynamicprogramming/practise/최대점수구하기.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class 최대점수구하기 {
 	public static void main(String[] args){
 		Scanner kb = new Scanner(System.in);
-		int n=kb.nextInt();
-		int m=kb.nextInt();
+		int[][] arr = {
+				{10, 5  },
+				{25, 12 },
+				{15, 8  },
+				{6 ,3   },
+				{7 ,4   }
+		};
+		int n= 5;//문제수
+		int m= 20;//제한시간
 		int[] dy=new int[m+1];
+		//문제를 순회한다.
 		for(int i=0; i<n; i++){
-			int ps=kb.nextInt();
-			int pt=kb.nextInt();
+			int ps=arr[i][0];//풀었을때점수
+			int pt=arr[i][1];//소요시간
+			//제한시간부터 역순회시작(내 소요시간까지)
 			for(int j=m; j>=pt; j--){
 				dy[j]=Math.max(dy[j], dy[j-pt]+ps);
 			}
@@ -18,8 +27,10 @@ public class 최대점수구하기 {
 		System.out.print(dy[m]);
 	}
 }
-/*
+/* 
+문제수/제한시간
 5 20
+좌,점수 우, 시간
 10 5
 25 12
 15 8
