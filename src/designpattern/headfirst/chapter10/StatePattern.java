3b28e3b8c47920cb76f07bd64a84c95033438edc
kks <rkwhr0010@naver.com>
2023-05-26 23:40:54 +0900

    상태패턴

--- src/designpattern/headfirst/chapter10/StatePattern.java
+++ src/designpattern/headfirst/chapter10/StatePattern.java
@@ -1,7 +1,6 @@
 package designpattern.headfirst.chapter10;
 
-public class StateTest2 {
-
+public class StatePattern {
 	static interface State {
 		void insertQuarter();
 
@@ -13,152 +12,131 @@ public class StateTest2 {
 
 		void refill();
 	}
+	
+	static abstract class BaseState implements State{
+		GumballMachine gumballMachine;
 
-	static class NoQuarterState implements State {
-		final GumballMachine gumballMachine;
-
-		public NoQuarterState(GumballMachine gumballMachine) {
+		public BaseState(GumballMachine gumballMachine) {
 			this.gumballMachine = gumballMachine;
 		}
-
+		
+	}
+	
+	static class NoQuarterState extends BaseState{
+		public NoQuarterState(GumballMachine gumballMachine) {
+			super(gumballMachine);
+		}
 		public void insertQuarter() {
-			System.out.println("동전을 넣으셨습니다.");
 			gumballMachine.setState(gumballMachine.getHasQuarterState());
+			System.out.println("동전을 넣으셨습니다.");
 		}
-
 		public void ejectQuarter() {
-			System.out.println("동전을 넣어 주세요.");
+			System.out.println("동전을 넣어 주세요");
 		}
-
 		public void turnCrank() {
 			System.out.println("동전을 넣어 주세요.");
 		}
-
 		public void dispense() {
 			System.out.println("동전을 넣어 주세요.");
 		}
-
 		public void refill() {
 		}
-
+		@Override
 		public String toString() {
-			return "waiting for quarter";
+			return "동전 기다리는 중";
 		}
 	}
-
-	static class HasQuarterState implements State {
-		GumballMachine gumballMachine;
-
+	static class HasQuarterState extends BaseState{
 		public HasQuarterState(GumballMachine gumballMachine) {
-			this.gumballMachine = gumballMachine;
+			super(gumballMachine);
 		}
-
 		public void insertQuarter() {
-			System.out.println("You can't insert another quarter");
+			System.out.println("동전은 한 개만 넣어 주세요.");
 		}
-
 		public void ejectQuarter() {
-			System.out.println("Quarter returned");
+			System.out.println("동전이 반환됩니다.");
 			gumballMachine.setState(gumballMachine.getNoQuarterState());
 		}
-
 		public void turnCrank() {
-			System.out.println("You turned...");
+			System.out.println("손잡이를 돌리셨습니다.");
 			gumballMachine.setState(gumballMachine.getSoldState());
 		}
-
 		public void dispense() {
-			System.out.println("No gumball dispensed");
-			
+			System.out.println("알맹이를 내보낼 수 없습니다.");
 		}
-
 		public void refill() {
 		}
-
+		@Override
 		public String toString() {
-			return "waiting for turn of crank";
+			return "손잡이 돌기는 것 기다리는 중";
 		}
 	}
-
-	static class SoldOutState implements State {
-		GumballMachine gumballMachine;
-
+	static class SoldOutState extends BaseState{
 		public SoldOutState(GumballMachine gumballMachine) {
-			this.gumballMachine = gumballMachine;
+			super(gumballMachine);
 		}
-
 		public void insertQuarter() {
-			System.out.println("You can't insert a quarter, the machine is sold out");
+			System.out.println("매진되었습니다. 다음 기회에 이용해 주세요.");
 		}
-
 		public void ejectQuarter() {
-			System.out.println("You can't eject, you haven't inserted a quarter yet");
+			System.out.println("매진되었습니다.");
 		}
-
 		public void turnCrank() {
-			System.out.println("You turned, but there are no gumballs");
+			System.out.println("손잡이를 돌렸지만, 알맹이는 없습니다.");
 		}
-
+	 
 		public void dispense() {
-			System.out.println("No gumball dispensed");
+			System.out.println("알맹이를 내보낼 수 없습니다.");
 		}
-
+		
 		public void refill() {
 			gumballMachine.setState(gumballMachine.getNoQuarterState());
 		}
-
+		@Override
 		public String toString() {
-			return "sold out";
+			return "매진";
 		}
 	}
-
-	static class SoldState implements State {
-
-		GumballMachine gumballMachine;
-
+	static class SoldState extends BaseState{
 		public SoldState(GumballMachine gumballMachine) {
-			this.gumballMachine = gumballMachine;
+			super(gumballMachine);
 		}
-
 		public void insertQuarter() {
-			System.out.println("Please wait, we're already giving you a gumball");
+			System.out.println("알맹이를 내보내고 있습니다.");
 		}
-
 		public void ejectQuarter() {
-			System.out.println("Sorry, you already turned the crank");
+			System.out.println("이미 알맹이를 뽑으셨습니다.");
 		}
-
 		public void turnCrank() {
-			System.out.println("Turning twice doesn't get you another gumball!");
+			System.out.println("손잡이는 한 번만 돌려 주세요.");
 		}
-
 		public void dispense() {
+		}
+		public void refill() {
 			gumballMachine.releaseBall();
 			if (gumballMachine.getCount() > 0) {
 				gumballMachine.setState(gumballMachine.getNoQuarterState());
 			} else {
-				System.out.println("Oops, out of gumballs!");
+				System.out.println("이런, 알맹이가 다 떨어졌어요");
 				gumballMachine.setState(gumballMachine.getSoldOutState());
 			}
 		}
-
-		public void refill() {
-		}
-
+		@Override
 		public String toString() {
-			return "dispensing a gumball";
+			return "알맹이 내보내는 중";
 		}
 	}
-
-	static class GumballMachine {
+	
+	
+	static class GumballMachine{
+		State state;
+		int count = 0;
+		
 		final State soldOutState;
 		final State noQuarterState;
 		final State hasQuarterState;
 		final State soldState;
-
-		State state;
-		int count = 0;
-
+		
 		public GumballMachine(int numberGumballs) {
 			soldOutState = new SoldOutState(this);
 			noQuarterState = new NoQuarterState(this);
@@ -167,30 +145,6 @@ public class StateTest2 {
 			
 			this.count = numberGumballs;
 			state = numberGumballs > 0 ? noQuarterState : soldOutState;
-		}
-		
-		public void insertQuarter() {
-			state.insertQuarter();
-		}
-		public void ejectQuarter() {
-			state.ejectQuarter();
-		}
-		public void turnCrank() {
-			state.turnCrank();
-			state.dispense();
-		}
-		int getCount() {
-			return count;
-		}
-	 
-		void refill(int count) {
-			this.count += count;
-			System.out.println("The gumball machine was just refilled; its new count is: " + this.count);
-			state.refill();
-		}
-		void releaseBall() {
-			System.out.println("A gumball comes rolling out the slot...");
-			if (count > 0) count -= 1;
 		}
 		void setState(State state) {
 			this.state = state;
@@ -214,21 +168,50 @@ public class StateTest2 {
 	    public State getSoldState() {
 	        return soldState;
 	    }
-	 
+		
+		
+		public void insertQuarter() {
+			state.insertQuarter();
+		}
+		
+		public void ejectQuarter() {
+			state.ejectQuarter();
+		}
+		
+		public void turnCrank() {
+			state.turnCrank();
+			state.dispense();
+		}
+		
+		public void refill(int count) {
+			this.count += count;
+			System.out.println("겜볼 기계가 방금 리필되었습니다. 개수는 다음과 같습니다." + this.count);
+			state.refill();
+		}
+		
+		public void releaseBall() {
+			System.out.println("알맹이가 슬롯에서 굴러 나옵니다....");
+			if (count > 0) {
+				count = count - 1;
+			}
+		}
+		public int getCount() {
+			return count;
+		}
+		
 		public String toString() {
 			StringBuffer result = new StringBuffer();
-			result.append("\nMighty Gumball, Inc.");
-			result.append("\nJava-enabled Standing Gumball Model #2004");
-			result.append("\nInventory: " + count + " gumball");
+			result.append("\n겜블 머신_");
+			result.append("\n자바 기반 Gumball Model #2004");
+			result.append("\n항목: " + count + " 알맹이");
 			if (count != 1) {
-				result.append("s");
+				result.append("들");
 			}
 			result.append("\n");
-			result.append("Machine is " + state + "\n");
+			result.append("머신 상태 :" + state + "\n");
 			return result.toString();
 		}
 	}
-
 	public static void main(String[] args) {
 		GumballMachine gumballMachine = new GumballMachine(2);
 
@@ -250,5 +233,5 @@ public class StateTest2 {
 
 		System.out.println(gumballMachine);
 	}
-
+	
 }
