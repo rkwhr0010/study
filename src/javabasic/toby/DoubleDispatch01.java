a26d86a9c0ea4bcdc0b66874e318de2fbed79a70
kks <rkwhr0010@gamil.com>
2023-05-31 12:17:14 +0900

    02 - 메서드 오버로딩으로 문제해결 시도

--- src/javabasic/toby/DoubleDispatch01.java
+++ src/javabasic/toby/DoubleDispatch01.java
@@ -4,39 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DoubleDispatch01 {
+	
 	static interface Post{
-		void postOn(SNS sns);
+		/*문제해결 시도 1 메서드 오버로딩 이용하기 */
+		void postOn(Facebook sns);
+		void postOn(Twitter sns);
 	}
 	static class Text implements Post{
-		public void postOn(SNS sns) {
-			if(sns instanceof Facebook) {
-				System.out.println("Text - >  Facebook");
-			}
-			if(sns instanceof Twitter) {
-				System.out.println("Text - >  Twitter");
-			}
-			/* if 문으로 타입체크 후 로직을 결정하면 생기는 단점
-			 * 새로운 SNS 추가 시 놓치기가 쉽다. 당장 이 예제에서도 
-			 * 동작은 하는데 if 문이 걸리는게 없어 예외도 발생하지 않는다.
-			 * 이게 더 큰 문제다. 놓치고 지나치면 언제 알아차릴지도 미지수
-			 * 놓치지 않기 위해 예외를 던지도록 설정을 한다면, 어찌됐건 런타임 시점에 문제가 된다.
-			 */
-			if(sns instanceof GooglePlus) {
-				System.out.println("Text - >  Twitter");
-			}
+		public void postOn(Facebook sns) {
+			System.out.println("Text - >  Facebook");
+		}
+		public void postOn(Twitter sns) {
+			System.out.println("Text - >  Twitter");
 		}
 	}
 	static class Picture implements Post{
-		public void postOn(SNS sns) {
-			if(sns instanceof Facebook) {
-				System.out.println("Picture - >  Facebook");
-			}
-			if(sns instanceof Twitter) {
-				System.out.println("Picture - >  Twitter");
-			}
-			/* 다른 곳에 GooglePlus를 처리했다고 생각했는데 빼먹을 수도 있다.
-			 * 이것도 심각한 로직 에러로 발견도 힘들다.
-			 */
+		public void postOn(Facebook sns) {
+			System.out.println("Picture - >  Facebook");
+		}
+		public void postOn(Twitter sns) {
+			System.out.println("Picture - >  Twitter");
 		}
 	}
 	
@@ -45,8 +32,6 @@ public class DoubleDispatch01 {
 	}
 	static class Twitter implements SNS{
 	}
-	static class GooglePlus implements SNS{
-	}
 	
 	public static void main(String[] args) {
 		List<Post> posts = Arrays.asList(new Text(),new Picture());
@@ -54,7 +39,11 @@ public class DoubleDispatch01 {
 		
 		for(Post p : posts) {
 			for(SNS s : sns) {
+				//오버로딩된 메서드 선택은 스테틱 디스패칭(컴파일 시점)을 통해서 이워진다.
+				//따라서 어떤 오버로딩된 메서드를 실행할지 몰라서 컴파일 시점에 에러가 난다.
 				p.postOn(s);
+//				p.postOn((Facebook) s); 아래 둘 중 뭐가 해당되는지 알수 없다.
+//				p.postOn((Twitter) s);
 			}
 		}
 	}
