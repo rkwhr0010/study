3b0ef2d232c40222fec94b753597bf3a9e568c07
kks <rkwhr0010@gamil.com>
2023-06-09 08:56:41 +0900

    복습

--- src/algorithm/sortsearching/practise/이분검색연습.java
+++ src/algorithm/sortsearching/practise/이분검색연습.java
@@ -12,6 +12,7 @@ public class 이분검색연습 {
 		System.out.println(Arrays.toString(arr));
 		System.out.println(target);
 		System.out.println(binarySearch2(arr, target));
+		System.out.println(bi(arr, target));
 		
 	}
 	
@@ -25,6 +26,19 @@ public class 이분검색연습 {
 			if(arr[mid]==target) return mid;
 			else if(arr[mid]<target) lt = mid+1;
 			else rt = mid-1;
+		}
+		return -1;
+	}
+	static int bi(int[] arr , int target) {
+		//정렬 필수
+		Arrays.sort(arr);
+		int lt = 0;
+		int rt = arr.length-1;
+		while(!(lt>rt)) {
+			int mid = (lt+rt)/2;
+			if(arr[mid] == target) return mid;
+			if(arr[mid]>target) rt = mid-1;
+			else lt = mid +1;
 		}
 		return -1;
 	}
