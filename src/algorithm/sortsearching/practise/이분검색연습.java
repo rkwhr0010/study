baa72d690afd1c8dcb3a8536cf6a4fc6b57ad2fc
kks <rkwhr0010@gamil.com>
2023-06-09 08:57:53 +0900

    이분검색 복습

--- src/algorithm/sortsearching/practise/이분검색연습.java
+++ src/algorithm/sortsearching/practise/이분검색연습.java
@@ -3,7 +3,21 @@ package algorithm.sortsearching.practise;
 import java.util.Arrays;
 
 import algorithm.sortsearching.RandomUtils;
+/*
+이분검색은 자료구조의 한 종류로, 자료의 중간값을 기준으로 자료의 탐색을 진행하는 검색 알고리즘입니다. 
+이분검색은 자료가 정렬되어 있을 때만 사용할 수 있는 알고리즘입니다.
 
+이분검색의 동작 과정은 다음과 같습니다.
+
+1. 자료의 중간값을 찾습니다.
+2. 중간값과 찾고자 하는 값을 비교합니다.
+3. 중간값이 찾고자 하는 값보다 크면, 중간값의 왼쪽 부분을 탐색합니다.
+4. 중간값이 찾고자 하는 값보다 작으면, 중간값의 오른쪽 부분을 탐색합니다.
+5. 자료의 모든 부분을 탐색할 때까지 2~4번의 과정을 반복합니다.
+
+이분검색은 자료의 크기가 n일 때, 최악의 경우에도 O(log n)의 시간 복잡도를 가지기 때문에, 
+자료의 크기가 큰 경우에도 효율적으로 탐색할 수 있습니다.
+ */
 public class 이분검색연습 {
 	public static void main(String[] args) {
 		int[] arr = RandomUtils.이분검색();
