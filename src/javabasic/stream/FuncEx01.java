cfc1d893693f30ba05d2d7658b45660a5a9b30b3
kks <rkwhr0010@gamil.com>
2023-06-19 14:15:32 +0900

    reduce() 오버로딩 및 리팩터링

--- src/javabasic/stream/FuncEx01.java
+++ src/javabasic/stream/FuncEx01.java
@@ -1,12 +1,10 @@
 package javabasic.stream;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -49,18 +47,22 @@ public class FuncEx01 {
 		
 		System.out.println("filter = "+
 				filter(users,user->user.age>30)+"\n"
-				+users
-				);
+				+users);
 		
 		System.out.println("map = "+
-				map(users, user->user.id)
-				);
+				map(users, user->user.id));
 		
 		each(Arrays.asList(1,2,3,4,5),System.out::print);
 		System.out.println();
+		
 		System.out.println("reduce = "+
-				reduce(Arrays.asList(1,2,3,4,5), (a,b)->a+b, Integer.valueOf(0))
-				);
+				reduce(Arrays.asList(1,2,3,4,5), (a,b)->a+b, Integer.valueOf(0)));
+		System.out.println("reduce = "+
+				reduce(Arrays.asList(1,2,3,4,5), (a,b)->a+b));
+		System.out.println("reduce = "+
+				reduce(Arrays.asList(1), (a,b)->a+b, Integer.valueOf(0)));
+		System.out.println("reduce = "+
+				reduce(Arrays.asList(1), (a,b)->a+b));
 		
 		
 	}
@@ -85,9 +87,7 @@ public class FuncEx01 {
 	 */
 	static <T,R> List<R> map(List<T> list, Function<T, R> mapper){
 		ArrayList<R> newList = new ArrayList<>();
-		each(list, data->{
-			newList.add(mapper.apply(data));
-		});
+		each(list, data->newList.add(mapper.apply(data)));
 		return newList;
 	}
 	/*
@@ -103,17 +103,22 @@ public class FuncEx01 {
 	 * 자바는 스트림 안에서 외부 변수를 참조하면 그 변수는 final 속성이 된다. 
 	 * 따라서 갱신하면서 누산을 하지 못해, 간접적으로 값을 수정해야 한다.
 	 */
-	static <T> T reduce(List<T> list,BiFunction<T, T, T> reducer ,T memo) {
-		//방법1 Type safety: Unchecked cast from Object 
-//		T[] result = (T[])Array.newInstance(memo.getClass(), 1);
-		//방법2
+	static <T> T reduce(List<T> list, BinaryOperator<T> reducer ,T memo) {
+		//간소화된 유효성 검사, 본질을 흐리지 않는 선에서 간략화
+		if(list.size() < 2) return list.get(0);
 		HashMap<Class<?>, T> map = new HashMap<>();
 		map.put(memo.getClass(), memo);
 		
-		each(list, data->{
-			map.compute(memo.getClass(), (k,v)->reducer.apply(v, data));
-		});
+		each(list, data->map.compute(memo.getClass(), (k,v)->reducer.apply(v, data)));
 		return map.get(memo.getClass());
+	}
+	/*
+	 * 자바는 람다에서 사용하는 변수는 final 속성을 띄기 때문에 memo 값 처리가 애매해진다.
+	 * 삼항연산자 등 별도 처리하기보다 오버로딩으로 처리했다.
+	 */
+	static <T> T reduce(List<T> list, BinaryOperator<T> reducer) {
+		if(list.size() < 2) return list.get(0);
+		return reduce(list.subList(1, list.size()), reducer, list.get(0));
 	}
 	
 }
