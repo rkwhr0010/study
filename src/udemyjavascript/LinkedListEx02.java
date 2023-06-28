7d19df250fb2935e6119becc8a411f26a4be7947
kks <rkwhr0010@naver.com>
2023-06-28 21:00:40 +0900

    unShift(), get() 구현

--- src/udemyjavascript/LinkedListEx02.java
+++ src/udemyjavascript/LinkedListEx02.java
@@ -28,7 +28,6 @@ public class LinkedListEx02 {
 			Node<T> newNode = new Node<>(val);
 			if(length == 0) {
 				head = newNode;
-				tail = newNode;
 			} else {
 				tail.next = newNode;
 				newNode.prev = tail;
@@ -78,19 +77,47 @@ public class LinkedListEx02 {
 		}
 		//첫자리에 삽입
 		DoublyLinkedList<T> unShift(T val) {
-			Node<T> newNode = new Node<>(val);
-			head.prev = newNode;
-			head = head.prev;
-			
 			if(length == 0) {
-				tail = newNode;
+				push(val);
+			} else {
+				Node<T> newNode = new Node<>(val);
+				//헤더 연결
+				newNode.next = head;
+				//헤더 이전에 신규 노드 연결
+				head.prev = newNode;
+				//이전 헤드를 신규 헤드로(신규 노드)
+				head = head.prev;
+				length++;
 			}
 			
 			return this;
 		}
 		//데이터 찾기
 		Node<T> get(int index){
-			return null;
+			Node<T> result = null;
+			if( !(0 <= index && index < length) ) {
+				return result;
+			}
+			
+			int start = 0;
+			int end = length-1 ;
+			//tail 시작
+			if(end >> 1 <= index) {
+				result = tail;
+				while(index != end) {
+					result = result.prev;
+					end--;
+				}
+			//head 시작
+			} else {
+				result = head;
+				while(index != start) {
+					result = head.next;
+					start++;
+				}
+			}
+			
+			return result;
 		}
 		
 		//데이터 수정
@@ -130,20 +157,25 @@ public class LinkedListEx02 {
 		DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
 		IntStream.range(1, 11).forEach(list::push);
 		System.out.println(list);
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list);
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list);
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list.shift());
-		System.out.println(list);
+		System.out.println(list.get(1));
+		System.out.println(list.get(3));
+		System.out.println(list.get(5));
+		System.out.println(list.get(6));
+		System.out.println(list.get(7));
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list);
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list);
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list.shift());
+//		System.out.println(list);
 		
 	}
 }
