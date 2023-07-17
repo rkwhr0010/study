d3a124ad3280d7a12b3a7b62d610fbfa46c4720c
kks <rkwhr0010@naver.com>
2023-07-17 23:21:19 +0900

    불필요한 코드 정리

--- src/javabasic/stream/StreamEx.java
+++
@@ -1,8 +0,0 @@
-package javabasic.stream;
-
-public class StreamEx {
-	//자바의 함수형 프로그래밍은 자바의 메모리할당과 GC 발전이 큰 역할을 했다. 
-	//스트림의 가장 큰 장점은 병렬 프로그래밍이 쉽다는 것
-	public static void main(String[] args) {
-	}
-}
