b2e47a0b4cc96ca2b1f13729f1bd3bb115e4f9e8
kks <rkwhr0010@gamil.com>
2023-07-17 17:45:12 +0900

    전략패턴 작성중

--- src/designpattern/behavioral/strategy/StrategeDriver.java
+++ src/designpattern/behavioral/strategy/StrategeDriver.java
@@ -3,11 +3,32 @@ package designpattern.behavioral.strategy;
 /**
  * <pre>
  * 전략 패턴을 사용하면 런타임에 가장 적합한 알고리즘을 선택할 수 있습니다.
- * 일반적으로 알고리즘을 적용하는 데 사용되는 인터페이스로 시작한 다음 가능한 각 알고리즘에 대해 여러 번 구현합니다.
+ * 인터페이스를 정의 후 여러 알고리즘을 구현한다.
  * 
  * 전략 패턴은 정책(Policy)이라고도 합니다.
+ * 
+ * # 구성요소
+ * Client
+ * 전략을 선택할 주체
+ * 클라이언트가 전략을 문맥에 설정하고, 실행하도록 명령한다.
+ * Context는 실제 전략을 수행하지만, 선택권은 없다
+ * 심지어 전략을 인터페이스로 다루기 때문에 어떤 전략을 수행하는지도 모른다.
+ * 
+ * Context
+ * 실제 전략을 실행할 문맥
+ * 외부로 전략 객체 설정자를 노출해야 한다.
+ * 클라이언트가 설정해준 전략을 실행(위임)한다. 구현을 알지 못한다.
+ * 
+ * Strategy
+ * 추상화된 인터페이스
+ * 
+ * ConcreteStrategy (1 ~ N)개
+ * 전략을 구현한 클래스
+ * 캡슐화된 알고리즘
+ * 
+ * # 키워드
+ * OCP, 런타임 
  * </pre>
- *
  */
 public class StrategeDriver {
 	public static void main(String[] args) {
@@ -28,6 +49,7 @@ public class StrategeDriver {
 	/**
 	 * <pre>
 	 * 1. 전략 인터페이스를 정의한다.
+	 * 추상화된 공통 행동을 정의한다.
 	 * </pre>
 	 */
 	static interface Strategy {
@@ -40,6 +62,7 @@ public class StrategeDriver {
 	/**
 	 * <pre>
 	 * 2. 구현체를 만든다. 
+	 * 알맞은 알고리즘을 구현한다.
 	 *  </pre>
 	 */
 	static class EventDiscount implements Strategy{
